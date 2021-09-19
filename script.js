369a30a7a0768c161a0de30efa4f6c371863a2ac
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2021-09-19 19:08:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const ulSquare = document.querySelector("ul.squares")
+
+for(let i=0; i<11;i++){
+    const li = document.createElement("li");
+
+    const random = (min,max) => Math.random() * (max - min ) + min
+    const size = Math.floor(random(10,120));
+    const position = random(1,99);
+    const delay = random(5, 0.1);
+    const duration = random(24, 12);
+    li.style.width = ``
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`
+    
+    li.style.left = `${position}%`;
+    li.style.animationDelay= `${delay}s`
+    li.style.animationDuration= `${duration}s`
+    
+    ulSquare.appendChild(li);
+
+
+}
